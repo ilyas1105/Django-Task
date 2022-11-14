5b085cc3bfdffb3b33e4ab4bb84989bc8069e1a5
Ilyas <opebiyiilyas@gmail.com>
2022-11-14 06:24:57 -0800

    Django Rest Framework

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,12 +1,12 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import viewsets
 from .models import Artiste, Song, Lyric
 from .serializers import ArtisteSerializer, SongSerializer, LyricSerializer
 
 # Create your views here.
-# def index(request):
-#     return HttpResponse("Hey! this is a music app")
+def index(request):
+    return HttpResponse("Hey! this is a music app")
 
 class ArtisteViewSet(viewsets.ModelViewSet):
     queryset = Artiste.objects.all()
